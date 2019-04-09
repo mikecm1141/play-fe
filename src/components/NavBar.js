442f1a268fd55f2c2ac28597cdce421cb47440a1
Mike <mikecm@gmail.com>
2019-04-09 17:44:09 -0600

    Use NavLink to track active page for CSS class

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { NavLink } from 'react-router-dom';
+
 import '../styles/navbar.css';
 
 export default class NavBar extends Component {
@@ -7,13 +9,19 @@ export default class NavBar extends Component {
     return(
       <nav className="bottom-nav">
         <div>
-          <a id="favorites" title="Favorite Songs" href="/favorites"><i className="far fa-heart fa-5x fa-fw"></i></a>
+          <NavLink to="/favorites" activeStyle="active">
+            <i className="far fa-heart fa-5x fa-fw"></i>
+          </NavLink>
         </div>
         <div>
-          <a id="search" title="Search Artists and Tracks" href="/"><i className="fas fa-search fa-5x fa-fw"></i></a>
+          <NavLink exact to="/" activeStyle="active">
+            <i className="fas fa-search fa-5x fa-fw"></i>
+          </NavLink>
         </div>
         <div>
-          <a id="playlists" title="View Your Playlists" href="/playlists"><i className="fas fa-music fa-5x fa-fw"></i></a>
+          <NavLink to="/playlists" activeStyle="active">
+            <i className="fas fa-music fa-5x fa-fw"></i>
+          </NavLink>
         </div>
       </nav>
     )
