52df829d7d94b4ebf55ad016bb70b251439cc348
Mike <mikecm@gmail.com>
2019-04-09 13:46:02 -0600

    WIP add track to favorites

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
 import '../styles/track.css';
 
-export default class Track extends Component {
+class Track extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      iconClass: 'favorite-button far fa-heart fa-1x fa-fw',
+      favorite: false
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { favorite } = this.state;
+    if(favorite) {
+      // Make API request to remove favorite
+      // Set favorite to false
+      // Set icon back to normal
+      this.setState({ 
+        iconClass: 'favorite-button far fa-heart fa-1x fa-fw',
+        favorite: false
+      });
+    } else {
+      // Make API request to set as favorite
+      // Set favorite to true
+      // Set icon to filled in
+      this.setState({ 
+        iconClass: 'favorite-button fas fa-heart fa-1x fa-fw',
+        favorite: true
+      });
+    }
+  }
+
   render() {
+    const { track_name } = this.props.track;
+    const { iconClass } = this.state;
     return(
       <div className='listed-song'>
-        <p className="name">{this.props.track.track_name}</p>
-        <a href="javascript:void(0)">
-          <i className="favorite-button far fa-heart fa-1x fa-fw"></i>
-        </a>
+        <div>
+          <p className="name">{track_name}</p>
+        </div>
+        <div>
+          <button onClick={this.handleClick}>
+            <i className={iconClass}></i>
+          </button>
+        </div>
       </div>
-    )
+    );
   }
 }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Track);
