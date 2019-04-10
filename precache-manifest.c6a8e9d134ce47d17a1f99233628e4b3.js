c9042bf08d9809ed3041bfccfdee3542f4844c55
Mike <mikecm@gmail.com>
2019-04-09 19:53:27 -0600

    Updates

---
+++ precache-manifest.c6a8e9d134ce47d17a1f99233628e4b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66371def4ea38a81ac7f",
+    "url": "/play-fe/static/js/runtime~main.0a84adc5.js"
+  },
+  {
+    "revision": "fd218d24b840c8d25142",
+    "url": "/play-fe/static/js/main.e9f5145b.chunk.js"
+  },
+  {
+    "revision": "876ae1896bcb59e39da4",
+    "url": "/play-fe/static/js/2.2e768a9e.chunk.js"
+  },
+  {
+    "revision": "fd218d24b840c8d25142",
+    "url": "/play-fe/static/css/main.ea178e79.chunk.css"
+  },
+  {
+    "revision": "64cfbb5b9bc5e9972e66933574217917",
+    "url": "/play-fe/index.html"
+  }
+];
